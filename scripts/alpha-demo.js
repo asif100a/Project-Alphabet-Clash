d21039119f8e6960f9105436a410e6c0150f4ef5
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-02-15 09:18:43 +0600

    Counting the Score & Life is added

--- scripts/alpha-demo.js
+++ scripts/alpha-demo.js
@@ -10,10 +10,60 @@
 // };
 
 
+function handleKeyboardButtonPress(event) {
+    const gamerPressed = event.key;
+
+    // get the expected to press
+    const displayAlphabet = document.getElementById('display-alphabet');
+    const targetAlphabet = displayAlphabet.innerText;
+    const lowerCaseAlphabet = targetAlphabet.toLowerCase();
+    // console.log('Gamer Pressed:', gamerPressed, 'target alphabet:', lowerCaseAlphabet);
+
+    // get matched Alphabet
+    if(gamerPressed === lowerCaseAlphabet) {
+        // get The Default Score number
+        const defaultScoreElement = document.getElementById('default-score');
+        const defaultScoreText = defaultScoreElement.innerText;
+        const defaultScoreInt = parseInt(defaultScoreText);
+        console.log(defaultScoreInt);
+
+        // Add new Score
+        const newScore = defaultScoreInt + 1;
+        
+        // Update the Score
+        defaultScoreElement.innerText = newScore;
+
+        // Start a new round
+        removeBackgroundById(lowerCaseAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('You losed your life');
+        // Step-1: get the default life number
+        const defaultLifeElement = document.getElementById('default-life');
+        const defaultLifeText = defaultLifeElement.innerText;
+        const defaultLifeInt = parseInt(defaultLifeText);
+
+        // Step-2: Reduce the life count
+        const newLife = defaultLifeInt - 1;
+
+        // Step-3: Display the updated life count
+        defaultLifeElement.innerText = newLife;
+    };
+};
+// Capture keyboard keypress
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame() {
     // Step-1: Generate a random alphabet 
     const alphabet = getARendomAlphabet();
-    console.log('Random Alphabet:', alphabet);
+
+    // Step-2: Display the Random Alphabet
+    const displayAlphabet = document.getElementById('display-alphabet');
+    displayAlphabet.innerText = alphabet;
+
+    // Step-3: Set Background color
+    setBackgroundById(alphabet);
 };
 
 function play() {
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,6 +8,16 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 };
 
+function setBackgroundById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+};
+
+function removeBackgroundById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+};
+
 function getARendomAlphabet() {
     // get Alphabet in an array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
